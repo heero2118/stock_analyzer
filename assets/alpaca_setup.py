13fa4dc0374ca9cc57fa50a083ca9c017771ed77
heero2118 <69662403+heero2118@users.noreply.github.com>
2025-04-06 10:39:03 -0400

    Delete assets/alpaca_setup.py

--- assets/alpaca_setup.py
+++
@@ -1,9 +0,0 @@
-# Alpaca API ####################################################################################
-from alpaca.data.historical import StockHistoricalDataClient
-from alpaca.data.requests import StockBarsRequest
-from alpaca.data.timeframe import TimeFrame
-
-# Alpaca keys
-api_key = 'AKARY3S28GJ1O05GWRVX'
-secret_key = 'WhECbTRozVhvjCfxjEzkMgbtrlD0NTCwnFgfftFF'
-client = StockHistoricalDataClient(api_key=api_key, secret_key=secret_key)
